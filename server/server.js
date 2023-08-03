5dadd9c3c755902c594168ddd96a23d4acdb3abc
Vaibhav Mahajan <vaibhavmahajan3003@gmail.com>
2023-08-03 20:10:27 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const app = express();
+
+app.listen(process.env.PORT,()=> console.log(`Server running on port: ${process.env.port}`));

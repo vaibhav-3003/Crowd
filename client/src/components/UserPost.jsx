a7e8a656eadff8e3d9ff49119ae71dca11e83c92
Vaibhav Mahajan <vaibhavmahajan3003@gmail.com>
2023-08-22 20:08:32 +0530

    client

--- client/src/components/UserPost.jsx
+++ client/src/components/UserPost.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { HeartIcon, ChatBubbleOvalLeftIcon } from "@heroicons/react/24/outline";
 
-const UserPost = ({imageSrc}) => {
+const UserPost = ({imageSrc,likes,comments}) => {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full relative group cursor-pointer">
       <img src={imageSrc} alt="post" className="aspect-square object-cover" />
+      <div className="hidden absolute inset-0 w-full h-full border bg-black bg-opacity-40 group-hover:block">
+        <div className="w-full h-full flex justify-center items-center gap-4 text-gray-200">
+          <div className="flex gap-1 items-center justify-center">
+            <HeartIcon className="w-7 h-7" />
+            <span className="text-lg">{likes}</span>
+          </div>
+          <div className="flex gap-1 items-center justify-center">
+            <ChatBubbleOvalLeftIcon className="w-7 h-7" />
+            <span className="text-lg">{comments}</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

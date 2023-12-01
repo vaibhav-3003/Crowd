d69bbc7a4f3aef142634e8a3bb022aa4d735f732
Vaibhav Mahajan <vaibhavmahajan3003@gmail.com>
2023-12-01 13:24:48 +0530

    client

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://crowd123.vercel.app/",
     credentials: true,
   })
 );
